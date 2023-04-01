b15844eda1cc853d4c069af1f91844344c42759e
Ivan <90147664+Brossend@users.noreply.github.com>
2023-04-01 18:33:47 +0600

    Update checkLocalStorage.ts

--- src/components/typescript/checkLocalStorage.ts
+++ src/components/typescript/checkLocalStorage.ts
@@ -3,6 +3,8 @@ import styles from '../CardOfQuote.module.css';
 function checkInLocalStorage(id: string) {
   const item: any = document.getElementById(id);
 
+  console.log('111');
+
   if (localStorage.getItem(id) === 'true') {
     if (item !== null) {
       item!.classList.add(styles.likeIconActive);
