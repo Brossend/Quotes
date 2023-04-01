fc322a8c34f85bce307cc6d6d37fdafcd7e4cf82
Ivan <90147664+Brossend@users.noreply.github.com>
2023-04-01 18:18:11 +0600

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,10 @@ import styles from './Header.module.css';
 function Header() {
   return (
     <div className={`row ${styles.header}`}>
-      <Link to="/" className={`col-2 col-sm-1 col-md-1 col-lg-1 ${styles.nav}`}>
+      <Link
+        to="/quotes/"
+        className={`col-2 col-sm-1 col-md-1 col-lg-1 ${styles.nav}`}
+      >
         Main
       </Link>
     </div>
